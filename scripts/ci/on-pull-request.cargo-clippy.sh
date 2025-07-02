a62193b4aa311e098d03e2348e25427e61f56595
Jeff Morrison <lbljeffmo@gmail.com>
2025-07-01 23:11:47 -0700

    [ci] Make clippy warnings fail CI

--- scripts/ci/on-pull-request.cargo-clippy.sh
+++ scripts/ci/on-pull-request.cargo-clippy.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 cd "${GITHUB_WORKSPACE}"
-# TODO: Make clippy warnings fail this CI job...
-#       (For now we have some "unused code" warnings that should eventually go
-#       away with more tests)
-#cargo clippy -- -Dwarnings
-cargo clippy
+cargo clippy --tests -- -Dwarnings
