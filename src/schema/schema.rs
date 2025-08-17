94f939bbcffd6bd1970b6ef930e505d900e83dee
Jeff Morrison <lbljeffmo@gmail.com>
2025-08-16 23:53:12 -0700

    Fix some dangling references in cargo docs

--- src/schema/schema.rs
+++ src/schema/schema.rs
@@ -24,33 +24,34 @@ impl Schema {
         &self.types
     }
 
-    /// Helper function that just delegates to [SchemaBuilder::new()].
+    /// Helper function that just delegates to [`SchemaBuilder::new()`].
     pub fn builder() -> SchemaBuilder {
         SchemaBuilder::new()
     }
 
-    /// Looks up a [Directive] by name.
+    /// Looks up a [`Directive`] by name.
     ///
     /// Note that this will return both schema-defined types as well as built-in
-    /// types like `"deprecated"` -> [Directive::Deprecated], `"include"` ->
-    /// [Directive::Include], etc.
+    /// types like `"deprecated"` -> [`Directive::Deprecated`], `"include"` ->
+    /// [`Directive::Include`], etc.
     pub fn lookup_directive(&self, directive_name: &str) -> Option<&Directive> {
         self.directive_defs.get(directive_name)
     }
 
-    /// Returns this [Schema]'s Mutation[^note] root operation type (if one was
+    /// Returns this [`Schema`]'s Mutation[^note] root operation type (if one was
     /// defined).
     ///
     /// [^note] It is ***strongly*** recommended that you use
-    /// [Schema::mutation_type()] in favor of looking for an [ObjectType] whose
-    /// name is `"Mutation"`. GraphQL [defines an object type named "Mutation"
-    /// as the _default_ Mutation type
-    /// ](https://spec.graphql.org/October2021/#sec-Root-Operation-Types.Default-Root-Operation-Type-Names),
+    /// [`Schema::mutation_type()`] in favor of looking for an
+    /// [`ObjectType`](crate::types::ObjectType) whose name is `"Mutation"`.
+    /// GraphQL [defines an object type named "Mutation" as the _default_
+    /// Mutation type ](https://spec.graphql.org/October2021/#sec-Root-Operation-Types.Default-Root-Operation-Type-Names),
     /// but it is aslo [possible to override this default
     /// ](https://spec.graphql.org/October2021/#RootOperationTypeDefinition) and
-    /// use a differently-named [ObjectType] instead. [Schema::mutation_type()]
-    /// factors in any such override and will return the _correct_ [ObjectType]
-    /// for this schema.
+    /// use a differently-named [`ObjectType`](crate::types::ObjectType)
+    /// instead. [`Schema::mutation_type()`] factors in any such override and
+    /// will return the _correct_ [`ObjectType`](crate::types::ObjectType) for
+    /// this schema.
     pub fn mutation_type(&self) -> Option<&GraphQLType> {
         self.mutation_type.as_ref().map(|named_ref| {
             named_ref.deref(self)
@@ -58,35 +59,37 @@ impl Schema {
         })
     }
 
-    /// Returns this [Schema]'s Query[^note] root operation type.
+    /// Returns this [`Schema`]'s Query[^note] root operation type.
     //
     /// [^note] It is ***strongly*** recommended that you use
-    /// [Schema::query_type()] in favor of looking for an [ObjectType] whose
-    /// name is `"Query"`. GraphQL [defines an object type named "Query" as the
-    /// _default_ Query type
-    /// ](https://spec.graphql.org/October2021/#sec-Root-Operation-Types.Default-Root-Operation-Type-Names),
+    /// [`Schema::query_type()`] in favor of looking for an
+    /// [`ObjectType`](crate::types::ObjectType) whose name is `"Query"`.
+    /// GraphQL [defines an object type named "Query" as the _default_ Query
+    /// type](https://spec.graphql.org/October2021/#sec-Root-Operation-Types.Default-Root-Operation-Type-Names),
     /// but it is aslo [possible to override this default
     /// ](https://spec.graphql.org/October2021/#RootOperationTypeDefinition) and
-    /// use a differently-named [ObjectType] instead. [Schema::query_type()]
-    /// factors in any such override and will return the _correct_ [ObjectType]
-    /// for this schema.
+    /// use a differently-named [`ObjectType`](crate::types::ObjectType)
+    /// instead. [`Schema::query_type()`] factors in any such override and will
+    /// return the _correct_ [`ObjectType`](crate::types::ObjectType) for this
+    /// schema.
     pub fn query_type(&self) -> &GraphQLType {
         self.query_type.deref(self)
             .expect("type is present in schema")
     }
 
-    /// Returns this [Schema]'s Subscription[^note] root operation type.
+    /// Returns this [`Schema`]'s Subscription[^note] root operation type.
     //
     /// [^note] It is ***strongly*** recommended that you use
-    /// [Schema::subscription_type()] in favor of looking for an [ObjectType]
-    /// whose name is `"Subscription"`. GraphQL [defines an object type named
-    /// "Subscription" as the _default_ Subscription type
-    /// ](https://spec.graphql.org/October2021/#sec-Root-Operation-Types.Default-Root-Operation-Type-Names),
+    /// [`Schema::subscription_type()`] in favor of looking for an
+    /// [`ObjectType`](crate::types::ObjectType) whose name is `"Subscription"`.
+    /// GraphQL [defines an object type named "Subscription" as the _default_
+    /// Subscription type](https://spec.graphql.org/October2021/#sec-Root-Operation-Types.Default-Root-Operation-Type-Names),
     /// but it is aslo [possible to override this default
     /// ](https://spec.graphql.org/October2021/#RootOperationTypeDefinition) and
-    /// use a differently-named [ObjectType] instead.
-    /// [Schema::subscription_type()] factors in any such override and will
-    /// return the _correct_ [ObjectType] for this schema.
+    /// use a differently-named [`ObjectType`](crate::types::ObjectType)
+    /// instead. [`Schema::subscription_type()`] factors in any such override
+    /// and will return the _correct_ [`ObjectType`](crate::types::ObjectType)
+    /// for this schema.
     pub fn subscription_type(&self) -> Option<&GraphQLType> {
         self.subscription_type.as_ref().map(|named_ref| {
             named_ref.deref(self)
