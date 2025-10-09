4394304636d65c337fc7faf2cd9f8d730db61302
Jeff Morrison <lbljeffmo@gmail.com>
2025-10-08 19:07:05 -0700

    [libgraphql-macros] New macros crate

--- crates/libgraphql/src/lib.rs
+++ crates/libgraphql/src/lib.rs
@@ -1,1 +1,8 @@
 pub use libgraphql_core::*;
+
+/// Helpful macros for doing things with GraphQL at Rust compile-time
+/// (e.g. Define a GraphQL [`Schema`](crate::schema::Schema) with
+/// compile-time GraphQL validation, etc)
+pub mod macros {
+    pub use libgraphql_macros::*;
+}
