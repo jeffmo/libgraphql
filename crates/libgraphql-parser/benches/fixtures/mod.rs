5ecf73242bb24a1f04c60b1921e9c4653813dd2f
Jeff Morrison <lbljeffmo@gmail.com>
2026-02-08 18:54:36 -0800

    [libgraphql-parser][benchmarks] Wire in third-party bechmarking fixtures

--- crates/libgraphql-parser/benches/fixtures/mod.rs
+++ crates/libgraphql-parser/benches/fixtures/mod.rs
@@ -8,5 +8,9 @@ pub const SIMPLE_QUERY: &str =
     include_str!("simple_query.graphql");
 pub const COMPLEX_QUERY: &str =
     include_str!("complex_query.graphql");
+pub const GITHUB_SCHEMA: &str =
+    include_str!("third-party/github-schema/schema.graphql");
+pub const STARWARS_SCHEMA: &str =
+    include_str!("third-party/starwars-schema/schema.graphql");
 
 pub mod operations;
