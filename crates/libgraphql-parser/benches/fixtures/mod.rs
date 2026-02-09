b75a2baef5f7f55ee5bdf4ba76311eb0286b3cc7
Jeff Morrison <lbljeffmo@gmail.com>
2026-02-08 17:59:54 -0800

    [libgraphql-parser][benchmarks] Initial fixtures

---
+++ crates/libgraphql-parser/benches/fixtures/mod.rs
@@ -0,0 +1,10 @@
+pub const SMALL_SCHEMA: &str =
+    include_str!("small_schema.graphql");
+pub const MEDIUM_SCHEMA: &str =
+    include_str!("medium_schema.graphql");
+pub const LARGE_SCHEMA: &str =
+    include_str!("large_schema.graphql");
+pub const SIMPLE_QUERY: &str =
+    include_str!("simple_query.graphql");
+pub const COMPLEX_QUERY: &str =
+    include_str!("complex_query.graphql");
