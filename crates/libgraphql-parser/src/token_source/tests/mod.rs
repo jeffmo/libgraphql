60b89e126822f244c321b4b1e207bf68fb282663
Jeff Morrison <lbljeffmo@gmail.com>
2026-02-11 16:45:46 -0800

    [libgraphql-parser][perf] B2: Unit tests for skip_whitespace() byte scanning

--- crates/libgraphql-parser/src/token_source/tests/mod.rs
+++ crates/libgraphql-parser/src/token_source/tests/mod.rs
@@ -2,4 +2,5 @@
 
 mod str_graphql_token_source_lex_name_byte_scanning_tests;
 mod str_graphql_token_source_peek_consume_ascii_fast_path_tests;
+mod str_graphql_token_source_skip_whitespace_byte_scanning_tests;
 mod str_to_graphql_token_source_tests;
